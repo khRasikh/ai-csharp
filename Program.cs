2f9240867f00b028a14fd5cf19117151603f7a07
khRasikh <kh.rasikh542@gmail.com>
2025-09-19 16:01:34 +0200

    adding conditions

--- Program.cs
+++ Program.cs
@@ -8,25 +8,39 @@
         {
             Console.WriteLine("Welcome to Main function!");
 
-            // variables 
-            Variables v = new Variables();
-            v.BooleanExample();
-            v.CharExample();
-            v.DecimalExample();
-            v.LongExample();
-            v.FloatExample();
-            v.StringExample();
-            v.ArrayExample();
+            // // variables 
+            // Variables v = new Variables();
+            // v.BooleanExample();
+            // v.CharExample();
+            // v.DecimalExample();
+            // v.LongExample();
+            // v.FloatExample();
+            // v.StringExample();
+            // v.ArrayExample();
 
 
-            // operators 
-            Operators o = new Operators();
-            o.ArithmeticOperators();
-            o.AssignmentOperators();
-            o.BitwiseOperators();
-            o.ComparisonOperators();
-            o.LogicalOperators();
-            o.UnaryOperators();
+            // // operators 
+            // Operators o = new Operators();
+            // o.ArithmeticOperators();
+            // o.AssignmentOperators();
+            // o.BitwiseOperators();
+            // o.ComparisonOperators();
+            // o.LogicalOperators();
+            // o.UnaryOperators();
+
+            // conditions 
+            IfCondition c = new IfCondition();
+            c.CheckEvenOdd(10);
+            c.CheckEvenOdd(11);
+            c.CheckEvenOdd(12);
+            c.CheckEvenOdd(15);
+            c.CheckNumber(10);
+            c.CheckNumber(-10);
+
+            // Switch
+            SwitchStatement s = new SwitchStatement();
+            string d = s.GetFirst("one");
+            Console.WriteLine(d);
         }
     }
 }
